07aa1d81bc67d44114d6fdd9c5badcf8e7af72b9
mhussainahmad <emhussain25@gmail.com>
2023-08-20 04:38:18 +0500

    Prepared base model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ChickenDisease import logger
 from ChickenDisease.pipeline.stage_01_data_ingestion import DataIngestionPipeline
-
+from ChickenDisease.pipeline.stage_02_prepare_base_model import PrepareBaseModelrainingPipeline
 
 
 STAGE_NAME = "STAGE 01: DATA INGESTION"
@@ -8,6 +8,19 @@ STAGE_NAME = "STAGE 01: DATA INGESTION"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "STAGE 02: PREPARE BASE MODEL"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = PrepareBaseModelrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
 except Exception as e:
